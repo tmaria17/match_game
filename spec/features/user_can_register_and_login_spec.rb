1cfc5ee6333c1bbb3acb38f3b075c01d711549ee
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-09-04 19:13:03 -0600

    Passing test for a user logging in.

--- spec/features/user_can_register_and_login_spec.rb
+++ spec/features/user_can_register_and_login_spec.rb
@@ -15,8 +15,23 @@ describe 'registration and login' do
       fill_in  :user_password, with: 'beesrcool'
       fill_in :user_location, with: 'Denver'
       click_on 'Create User'
-      save_and_open_page
+      #save_and_open_page
       expect(page).to have_content(username)
+    end
+  end
+  describe 'login' do
+    it 'allows a visitor to login' do
+      user= User.create(username: "Aboringusername", location: "Denver", password: "dogsrthebest")
+      visit new_user_path
+      click_on 'I already have an account'
+      expect(current_path).to eq(login_path)
+
+      fill_in :username, with: user.username
+      fill_in :password, with: user.password
+
+      click_on 'Log in'
+      expect(current_path).to eq(user_path(user))
+      expect(page).to have_content("#{user.username}")
     end
   end
 end
