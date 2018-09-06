56146a79b1e31f70a03cd1cd1b7f3254fe8bdc88
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-09-05 22:53:26 -0600

    Creates seeds for heroku.

--- app/controllers/board_games_controller.rb
+++ app/controllers/board_games_controller.rb
@@ -1,7 +1,7 @@
 class BoardGamesController < ApplicationController
   def index
     #@user = User.find(params[:user_id])
-    @boardgames= BoardGame.all
+    @boardgames= BoardGame.all.uniq
   end
 
   def show
